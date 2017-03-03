f31d369cb7ea4b70cc0c50e7f49280be695a5fa4
bushitan <373514952@qq.com>
2017-03-03 16:05:36 +0800

    v 3.2.7 可能用了假证书，跳过SSL认证

--- image_server/wx_app/lib/utils/qiniu_url_add.py
+++ image_server/wx_app/lib/utils/qiniu_url_add.py
@@ -44,6 +44,7 @@ def img_resize(img_local_path):
 	subprocess.check_output(_cmd, shell=True)
 	return  info["type"].lower()
 
+import ssl
 USER_CATEGORY = "1"
 URL  = SETTING.BASE_HOST + "upload/token/"
 # URL  = "http://192.168.199.203:8000/upload/token/"
@@ -57,7 +58,8 @@ def img_upload(qiniu_path,img_type,img_path,session):
 		"category_id":USER_CATEGORY,
 	}
 	params = urllib.urlencode(params)
-	ret = urllib.urlopen("%s?%s"%(URL, params))
+	context = ssl._create_unverified_context() #跳过SSL认证~~
+	ret = urllib.urlopen("%s?%s"%(URL, params),context = context)
 	ret_read =  ret.read().decode("utf-8-sig")
 	ret_json = json.loads( ret_read)
 
