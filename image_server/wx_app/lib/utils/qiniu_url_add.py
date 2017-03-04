fb9d07cb38f31099a11305bf403a55ddd642c661
bushitan <373514952@qq.com>
2017-03-04 09:59:23 +0800

    v 3.2.9 直传今日斗图目录

--- image_server/wx_app/lib/utils/qiniu_url_add.py
+++ image_server/wx_app/lib/utils/qiniu_url_add.py
@@ -49,7 +49,7 @@ def img_resize(img_local_path):
 	return  info["type"].lower()
 
 import ssl
-USER_CATEGORY = "1"
+USER_CATEGORY = "2" #今日斗图目录
 URL  = SETTING.BASE_HOST + "upload/token/"
 # URL  = "http://192.168.199.203:8000/upload/token/"
 def img_upload(qiniu_path,img_type,img_path,session):
